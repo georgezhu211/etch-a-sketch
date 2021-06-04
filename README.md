d65c3486124aebe752c252897e6cc7b7f5599cd0
George <georgezhu211@gmail.com>
2021-06-04 15:46:33 -0500

    add README.md and edit CSS

---
+++ README.md
@@ -0,0 +1,15 @@
+# Etch-A-Sketch
+
+A Etch-A-Sketch game
+
+## Description
+
+A Etch-A-Sketch game built with JavaScript that you can play in the browser
+
+## Visuals
+
+![preview](preview.png)
+
+## Acknowledgment
+
+[The Odin Project](https://www.theodinproject.com/)
--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,24 @@
 const container = document.querySelector('.container');
 
 function generateGrid(num) {
-    squared = num ** 2
-    for(let i = 0 ; i < squared; i++) {
-        const box = document.createElement('div');
-        box.classList.toggle('box');
-        box.style.width = (720/num).toString() + "px";
-        box.style.height = (720/num).toString() + "px";
-        container.appendChild(box);
-    }
+  squared = num ** 2;
+  for (let i = 0; i < squared; i++) {
+    const box = document.createElement('div');
+    box.classList.toggle('box');
+    box.style.width = (720 / num).toString() + 'px';
+    box.style.height = (720 / num).toString() + 'px';
+    box.style.opacity = 0;
+    container.appendChild(box);
+  }
 }
 
 function addHover() {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        box.addEventListener('mouseover', () => {
-            box.style.opacity = +box.style.opacity + 0.1;
-        });
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+      box.style.opacity = +box.style.opacity + 0.1;
     });
-}
- 
-function randomColor() {
-    const randomColor = MMath.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
+  });
 }
 
 generateGrid(4);
@@ -32,27 +28,36 @@ const clearBtn = document.querySelector('.clear-btn');
 const rainbowBtn = document.querySelector('.rainbow-btn');
 
 clearBtn.addEventListener('click', () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        box.style.opacity = 0;
-    });
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    box.style.opacity = 0;
+  });
 });
 
 newBtn.addEventListener('click', () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        container.removeChild(box)
-    });
-    let answer = prompt("Enter a number between 2-100: ");
-    generateGrid(answer);
-    addHover();
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    container.removeChild(box);
+  });
+  let answer = prompt('Enter a number between 2-100: ');
+  generateGrid(answer);
+  addHover();
 });
 
 rainbowBtn.addEventListener('click', () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        box.style.backgroundColor = randomColor;
-        box.style.opacity = 0;
-    });
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    const randomColor = [
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'indigo',
+      'violet',
+    ];
+    box.style.backgroundColor =
+      randomColor[Math.floor(Math.random() * randomColor.length)];
+    box.style.opacity = 0;
+  });
 });
